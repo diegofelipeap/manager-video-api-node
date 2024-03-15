ebbce9c7c4d3c275a8095cd33d94ad8ff365cc36
diegofelipeap <diegospd22@gmail.com>
2024-03-14 23:31:28 -0300

    Instalando nova extensão e trabalhando com rotas.

--- database-memory.js
+++ database-memory.js
@@ -1,3 +1,20 @@
-export class dataBaseMemory{
-    #videos = [] //chaves privadas
+import { randomUUID } from "node:crypto";
+export class dataBaseMemory {
+    #videos = new Map()
+
+    list() {
+        return this.#videos.values()
+    }
+
+    create(video) {
+        const videoId = randomUUID()
+        this.#videos.set(videoId, video)
+    }
+    update(id, video) {
+        this.#videos.set(id, video)
+    }
+    delete(id) {
+        this.#videos.delete(id)
+    }
+
 }
--- server.js
+++ server.js
@@ -1,18 +1,30 @@
+//API gerenciadora de vídeos. Métodos HTTP e CRUD.
+
 import { fastify } from "fastify"
 import { dataBaseMemory } from "./database-memory.js"
 
 const server = fastify()
 
-server.post('/videos', () => {
-    return `Server started ✅`
+const database = new dataBaseMemory()
+
+server.post('/videos', (request, reply) => {
+    database.create({
+        tittle: 'Video teste #01',
+        description: 'Este é um vídeo para testar o método POST.',
+        duration: 180
+    })
+
+    return reply.status(201).send()
 })
+
+
 server.get('/videos', () => {
     return `Server started ✅`
 })
-server.put('/videos', () => {
+server.put('/videos/:id', () => {
     return `Server started ✅`
 })
-server.delete('/videos', () => {
+server.delete('/videos/:id', () => {
     return `Server started ✅`
 })
 
